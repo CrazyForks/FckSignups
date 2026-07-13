bc1f18e054c16d781a6e7a164ddd20a3cd2e6ca3
BraveOPotato <abdullahalkafajy321@gmail.com>
2026-07-13 06:18:30 -0500

    chore(report button): changed report button to better reflect purpose

--- src/components/Report.tsx
+++ src/components/Report.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Toast } from "../components/Toast";
 import { useReport } from "../hooks/useReport";
 import { useModal } from "../hooks/useModal";
+import { ChatIcon } from "../constants/icons";
 
 export function Report() {
   const { reportMode, setReportMode } = useReport();
@@ -35,21 +36,7 @@ function ReportButton({
         className="submit-tool-button report-button"
         data-sticky={clicked}
       >
-        <svg
-          style={{ display: "block" }}
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <line x1="22" y1="2" x2="11" y2="13"></line>
-          <polygon points="22 2 15 22 11 13 2 9 22 2"></polygon>
-        </svg>
+        <ChatIcon />
       </button>
     </>
   );
